ca4dae407e7d4f2f851ad875d76feb1bd8fe2c3c
euclidesmendonca <euclidesmendonca.f@gmail.com>
2024-01-31 15:31:03 -0500

    Update Class_1.R

--- Class_1.R
+++ Class_1.R
@@ -185,6 +185,249 @@ chart.Correlation(
 # Saving the dataset ####
 write_sas(data = student.heights, path = "Undergraduate_anthropometric_updated.sav")
 
+####################
+#### Session 2 ####
+##################
+
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("rcompanion")
+library(rcompanion) # used for obtaining standard errors and adjusted means
+
+library(psych)
+
+install.packages("PerformanceAnalytics")
+library(PerformanceAnalytics)
+
+install.packages("ggsignif")
+library(ggsignif)
+
+# Set the working directory
+setwd("C:/Users/Euclides/Desktop/GSAP_Workshop/R_Programing_Workshop/")
+getwd()
+
+# Load dataset
+
+wm <- read.csv("Working_memory.csv")
+wm <- read.csv("https://github.com/euclidesmendonca/R_Programing_Workshop/raw/main/Working_memory.csv")
+
+library(haven)
+heights <- read_spss("Undergraduate_anthropometric.sav")
+
+plot(heights$height ~ heights$mother_height)
+
+#### Grammar of Graphics - GGPLOT2 #####
+
+jpeg(filename = "My_scatter_Plot.jpeg",
+     width = 12,
+     height = 5*2,
+     units = "cm",
+     res = 450)
+
+ggplot(data = heights, aes(x=mother_height, y= height)) +
+  geom_point() +
+  theme_bw()   +
+  labs(title = "My scatter plot",
+       subtitle = "Height in centimeters",
+       x= "Mother's height",
+       y = "Students' height"
+       )+
+  theme(axis.title = element_text(hjust = .5),
+        plot.title = element_text(hjust = .5) )+
+  geom_smooth(method = 'lm' )
+
+dev.off() # This tells R to end the graph device
+
+# Saving PDF
+pdf(file = "My_Scatter_Plots.pdf", width = 12, height = 10, onefile = TRUE)
+ggplot(data = heights, aes(x=mother_height, y= height)) +
+  geom_point() +
+  theme_bw()   +
+  labs(title = "My scatter plot",
+       subtitle = "Height in centimeters",
+       x= "Mother's height",
+       y = "Students' height"
+  )+
+  theme(axis.title = element_text(hjust = .5),
+        plot.title = element_text(hjust = .5) )+
+  geom_smooth(method = 'lm' )
+
+
+ggplot(data = heights, aes(x=sex, y= height)) +
+  geom_boxplot() +
+  geom_point()+
+  geom_jitter()+
+  theme_bw()  
+
+dev.off()
+
+# Exploring Boxplots and distributions
+
+ggplot(data = heights, aes(x=sex, y= height, fill = sex)) +
+  geom_boxplot() +
+  geom_point()+
+  geom_jitter()+
+  theme_bw()
+
+# Recoding the sex variable
+heights$sex <- ifelse( heights$sex == "masculino",
+                   yes = "Males",
+                   no = "Females"
+                   )
+
+ggplot(data = heights, aes(x=sex, y= height, fill = sex)) +
+  geom_boxplot() +
+  geom_point()+
+  geom_jitter()+
+  theme_bw()
+
+ggplot(data = heights, aes(x=sex, y= height, fill = sex)) +
+  geom_violin() +
+  geom_point()+
+  geom_jitter()+
+  theme_bw()+
+  theme(legend.position = c( .2, .85 ) )+
+  scale_fill_manual(values = c('#cf463c', '#42789e'))
+
+# Creating Bar graphs with standard errors
+
+ggplot(data = heights, aes(x=sex, y= height, fill = sex))+
+  geom_bar(stat = "summary")+
+  theme_bw()+
+  theme(legend.position = c( .2, .85 ) )+
+  scale_fill_manual(values = c('#cf463c', '#42789e'))+
+  coord_cartesian( ylim = c(50, 200 ) )
+
+ggplot(data = heights, aes(x=sex, y= height, fill = sex))+
+  geom_bar(stat = "summary")+
+  theme_bw()+
+  theme(legend.position = c( .2, .85 ) )+
+  scale_fill_manual(values = c('#cf463c', '#42789e'))+
+  ylim(c(50, 200 )) # Not advisable because it removes data points
+
+# Bar graphs with  standard errors
+library(rcompanion)
+descriptive_stats <- groupwiseMean( height ~ sex, data = heights )
+descriptive_stats
+
+ggplot(data = descriptive_stats, aes(x = sex, y = Mean, fill=sex ))+
+  geom_bar(stat = 'identity', alpha = .40)+
+  geom_errorbar(aes( ymin= Trad.lower, ymax =  Trad.upper), width = .1)+
+  coord_cartesian(ylim = c(100 ,200))+
+  theme_bw()+
+  theme(legend.position = c(.15, .85))+
+  scale_fill_manual(values = c('#cf463c', '#42789e'))+
+  geom_point(data = heights,
+             aes(x= sex, y = height),
+             position = position_jitter(.1)
+             )
+
+########################
+#### Linear Models ####
+######################
+
+psych::describeBy( heights$height, group = heights$sex)
+
+
+fit.height <-  lm(formula =  height ~ sex, data = heights )
+summary(fit.height)
+
+ggplot(data = heights, aes(x = sexo_dummy , y = height))+
+  geom_point()+
+  geom_smooth(method = 'lm')
+
+162.5556 + 15.7958
+  
+t.test( height ~ sex , data = heights, var.equal=TRUE)
+  
+# Add covariables - multiple regression
+
+fit.height2 <- lm(formula = height ~ sex + mother_height, data=heights)
+summary(fit.height2)
+
+fit.height3 <- lm(formula = height ~ sex + mother_height + fathers_height,
+                  data=heights)
+summary(fit.height3)
+
+
+fit.height.males <- lm(formula = height ~ mother_height ,
+                       data= heights[ heights$sex == "Males", ] )
+summary(fit.height.males)
+
+fit.height.females <- lm(formula = height ~ mother_height ,
+                         data=heights[ heights$sex == "Females", ])
+summary(fit.height.females)
+
+ggplot(data = heights, aes(x = mother_height, y = height))+
+  geom_point()+
+  geom_smooth(method='lm')+
+  facet_wrap(~sex)
+
+# Testing the interaction between sex and mother's height
+fit.height.interact <- lm(formula = height ~ sex + mother_height + sex : mother_height,
+                          data= heights  )
+
+fit.height.interact <- lm(formula = height ~ sex * mother_height,
+                          data= heights  )
+
+summary(fit.height.interact)
+anova(fit.height.interact)
+
+# Executive Function Dataset
+colnames(wm)
+
+fit.IQ <- lm ( IQ ~ Exp.group, data = wm)
+summary(fit.IQ)
+
+ggplot(data=wm, aes(x = Exp.group, y = IQ))+
+  geom_boxplot()+
+  geom_point()
+
+# Checking if Working memory is protective of threat condition
+
+fit.IQ.mod <- lm ( IQ ~ Exp.group * Working.memory, data = wm)
+
+ggplot(data = wm, aes( x = Working.memory, y = IQ, col = Exp.group ))+
+  geom_point()+
+  geom_smooth(method = 'lm')
+
+summary(fit.IQ.mod )
+  
+library(interactions)
+interactions::sim_slopes(fit.IQ.mod, pred = Working.memory, modx = Exp.group)
+
+ggplot(data = wm, aes( x = Working.memory, y = IQ, col = Exp.group ))+
+  geom_point()+
+  geom_smooth(method = 'lm')+
+  theme_bw()+
+  theme(legend.position = c(.15, .85))+
+  annotate(geom = 'text', x = 140, y = 60,label= "b = .59, p<.001" )+
+  annotate(geom = 'text', x = 140, y = 110,label= "b = .12, p = 0.34" )
+
+cor.test(wm[wm$Exp.group == 'control',]$IQ,
+         wm[wm$Exp.group == 'control', ]$Working.memory) 
+
+cor.test(wm[wm$Exp.group == 'threat1',]$IQ,
+         wm[wm$Exp.group == 'threat1', ]$Working.memory) 
+
+ggplot(data = wm, aes( x = Working.memory, y = IQ, col = Exp.group ))+
+  geom_point()+
+  geom_smooth(method = 'lm')+
+  theme_bw()+
+  theme(legend.position = c(.15, .85))+
+  annotate(geom = 'text', x = 140, y = 60,label= "r = 0.72, p<.001" )+
+  annotate(geom = 'text', x = 140, y = 110,label= "r = 0.10, p = 0.45" )
+
+# Running an anova would look like this
+fit.aov <- lm( IQ ~ Exp.group, data = wm)
+anova(fit.aov)
+
+
+
+
+
+
 
 
 
